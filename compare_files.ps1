143febf68578aaf9fc1ff28ef2133919da3b8062
Vinutna Nandyala <vinuthna.nandyala@gmail.com>
2020-03-12 18:23:42 +0530

    added powershell compare file

---
+++ compare_files.ps1
@@ -0,0 +1,23 @@
+﻿$Folder1 = "C:\Users\804175\Documents\logs"
+$Folder2 = "D:\logs"
+$Folder3 = "D:\Vinu"
+
+$Folder1files = Get-ChildItem -Path $Folder1 -Recurse
+$Folder2files = Get-ChildItem -Path $Folder2 -Recurse
+$Filediff =Compare-Object $Folder1files $Folder2Files -Property Name, Length, LastWriteTime -passthru 
+
+$Filediff | foreach {
+
+ $copyparams = @{
+     'Path' = $_.InputObject.Fullname
+}
+ 
+ if ($_.SideIndicator -eq '<=')
+ {
+   $copyparams.Destination = $Folder3
+ }
+ 
+ }
+ Copy-Item @copyparams
+}
+
